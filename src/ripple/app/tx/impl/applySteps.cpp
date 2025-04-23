9d09ee12b522f372b3684ee1508cb8162ac98183
tequ <git@tequ.dev>
2025-04-23 21:33:33 +0900

    Remove #ifndef DEBUG guards and exception handling wrappers

--- src/ripple/app/tx/impl/applySteps.cpp
+++ src/ripple/app/tx/impl/applySteps.cpp
@@ -392,19 +392,15 @@ preflight(
 {
     PreflightContext const pfctx(app, tx, rules, flags, j);
 
-#ifndef DEBUG
     try
     {
-#endif
         return {pfctx, invoke_preflight(pfctx)};
-#ifndef DEBUG
     }
     catch (std::exception const& e)
     {
         JLOG(j.fatal()) << "apply: " << e.what();
         return {pfctx, {tefEXCEPTION, TxConsequences{tx}}};
     }
-#endif
 }
 
 PreclaimResult
@@ -441,21 +437,17 @@ preclaim(
             preflightResult.j);
     }
 
-#ifndef DEBUG
     try
     {
-#endif
         if (!isTesSuccess(ctx->preflightResult))
             return {*ctx, ctx->preflightResult};
         return {*ctx, invoke_preclaim(*ctx)};
-#ifndef DEBUG
     }
     catch (std::exception const& e)
     {
         JLOG(ctx->j.fatal()) << "apply: " << e.what();
         return {*ctx, tefEXCEPTION};
     }
-#endif
 }
 
 XRPAmount
@@ -479,10 +471,8 @@ doApply(PreclaimResult const& preclaimResult, Application& app, OpenView& view)
         // info to recover.
         return {tefEXCEPTION, false};
     }
-#ifndef DEBUG
     try
     {
-#endif
         if (!preclaimResult.likelyToClaimFee)
             return {preclaimResult.ter, false};
 
@@ -495,14 +485,12 @@ doApply(PreclaimResult const& preclaimResult, Application& app, OpenView& view)
             preclaimResult.flags,
             preclaimResult.j);
         return invoke_apply(ctx);
-#ifndef DEBUG
     }
     catch (std::exception const& e)
     {
         JLOG(preclaimResult.j.fatal()) << "apply: " << e.what();
         return {tefEXCEPTION, false};
     }
-#endif
 }
 
 }  // namespace ripple
